e0c1ebc9e577c6f166cb4a275a271b56789838a0
Thulile <THULILEMASUKU@YAHOO.COM>
2025-08-31 21:40:41 +0200

    Fix imports and file locations for auto-checker

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,17 @@
+import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   return (
-    <>
+    <div>
       <WelcomeMessage />
-    </>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
   );
 }
 
