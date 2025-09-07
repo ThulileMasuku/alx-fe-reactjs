c82df3c1da3aa9275b9fd0ae331b350454f79694
Thulile <THULILEMASUKU@YAHOO.COM>
2025-09-07 21:34:19 +0200

    Add Header, MainContent, and Footer components and update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
-import WelcomeMessage from './components/WelcomeMessage';
 import './App.css';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
