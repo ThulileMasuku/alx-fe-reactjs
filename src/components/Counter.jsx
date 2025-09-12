fcc636fc063bf96a1beaabe0361a83878a4c0013
Thulile <THULILEMASUKU@YAHOO.COM>
2025-09-12 15:03:15 +0200

    Add Counter component and intergrate in App

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,7 +5,7 @@ function Counter() {
 
   return (
     <div>
-      <p>Current Count: {count}</p>
+      <p>Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
       <button onClick={() => setCount(0)}>Reset</button>
