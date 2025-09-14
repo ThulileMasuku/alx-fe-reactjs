c2dd14f709d540a08ff1155c17562540f053d6fb
Thulile <THULILEMASUKU@YAHOO.COM>
2025-09-14 23:34:41 +0200

    Fix Navbar.jsx inline styling: add backgroundColor, display, justifyContent

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,13 +3,21 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   const navStyle = {
-    margin: "10px",
+    margin: "0 10px",
     textDecoration: "none",
-    color: "blue"
+    color: "white"
+  };
+
+  const navBarContainer = {
+    display: "flex",               // required
+    justifyContent: "space-around", // required
+    alignItems: "center",
+    padding: "10px",
+    backgroundColor: "#007BFF"     // required
   };
 
   return (
-    <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
+    <nav style={navBarContainer}>
       <Link style={navStyle} to="/">Home</Link>
       <Link style={navStyle} to="/about">About</Link>
       <Link style={navStyle} to="/services">Services</Link>
