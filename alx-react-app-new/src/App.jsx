54ac202c0ae32f32a98dd253bc4f3c482094a8f0
Thulile <THULILEMASUKU@YAHOO.COM>
2025-09-20 11:56:04 +0200

    Initial commit with all sub-projects

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,13 +1,8 @@
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
+import Counter from './components/Counter';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Counter />
   );
 }
 
--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -22,3 +22,4 @@ function UserProfile(props) {
 }
 
 export default UserProfile;
+
