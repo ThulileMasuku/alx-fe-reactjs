9c1818d14f1908ca8f64a504465a31ef9383ab8b
Thulile <THULILEMASUKU@YAHOO.COM>
2025-10-12 18:43:43 +0200

    Fix: removed nested git repositories and added all project folders

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <main
+      style={{
+        padding: "20px",
+        backgroundColor: "#f9f9f9",
+        textAlign: "center",
+      }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
